bad6b612c1c4235640f6d7990831f8a469e32ab5
Justin Burr <juburr@users.noreply.github.com>
2024-02-08 18:10:17 +0000

    feat: add an install, sign, and verify command

---
+++ src/scripts/sign_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Load the private key, normally a base64 encoded secret within a CircleCI context
+echo "${PARAM_PRIVATE_KEY}" | base64 --decode > cosign.key
+
+# Sign the image using its digest
+IMAGE_DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' "${PARAM_IMAGE}")
+echo "Signing ${IMAGE_DIGEST}..."
+cosign sign --key cosign.key "${IMAGE_DIGEST}"
+
+# As an precautionary measure, destroy the private key at this point
+shred -vzu -n 10 cosign.key
+unset PARAM_PRIVATE_KEY
