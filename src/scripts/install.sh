81e594fd5eb0113bcd20fdc3f2c33d951a15e4fe
Justin Burr <20321959+juburr@users.noreply.github.com>
2024-08-05 08:31:30 -0400

    fix: better handling of secrets and orb paremeters with environment variables (#15)
    Co-authored-by: Justin Burr <juburr@users.noreply.github.com>

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -2,84 +2,9 @@
 
 set -e
 
-# Check to see if there are any environment variables within another environment
-# variable, and if so, expand them. This allows environment variables to be passed
-# as arguments to CircleCI orbs, which often don't interpret those variables correctly.
-# Although this function seems complex, it's safer than doing: VAR=$(eval echo "${VAR}")
-# and is less susceptible to command injection.
-#   - Supported: ${VARIABLE}
-#   - Unsupported: $VARIABLE
-expand_circleci_env_vars() {
-    search_substring=${1}
-    result=""
-
-    regex="([^}]*)[$]\{([a-zA-Z_]+[a-zA-Z0-9_]*)\}(.*)"
-    while [[ $search_substring =~ $regex ]]; do
-        prefix=${BASH_REMATCH[1]}
-        match=${BASH_REMATCH[2]}
-        suffix=${BASH_REMATCH[3]}
-
-        # if the environment variable exists, evaluate it, but
-        # guard against infinite recursion. e.g., MYVAR="\$MYVAR"
-        if [[ -n ${!match} ]] && [[ "${!match}" != "\${${match}}" ]]; then
-            repaired="${prefix}${!match}"
-            result="${result}${repaired}"
-            search_substring="${suffix}"
-        else
-            result="${result}${prefix}"
-            search_substring="${suffix}"
-        fi
-    done
-
-    midpoint_result="${result}${search_substring}"
-    search_substring="${result}${search_substring}"
-    result=""
-    env_var_present=false
-
-    # Handle the non-squiggley brace syntax: $VARIABLE.
-    regex="([^$]*)[$]([a-zA-Z_]+[a-zA-Z0-9_]*)(.*)"
-    while [[ $search_substring =~ $regex ]]; do
-        prefix=${BASH_REMATCH[1]}
-        match=${BASH_REMATCH[2]}
-        suffix=${BASH_REMATCH[3]}
-
-        # if the environment variable exists, evaluate it, but
-        # guard against infinite recursion. e.g., MYVAR="\$MYVAR"
-        if [[ -n ${!match} ]] && [[ "${!match}" != "\$${match}" ]]; then
-            repaired="${prefix}${!match}"
-            result="${result}${repaired}"
-            search_substring="${suffix}"
-            env_var_present=true
-        else
-            # If completely unset and not just an empty value, just leave it be
-            # to deal with inadequacies of aggressive mode. If the variable is
-            # actually present, but has an empty value, replace it with "".
-            if [[ -z ${!match+x} ]]; then
-                result="${result}${prefix}\$${match}"
-            else
-                result="${result}${prefix}"
-                env_var_present=true
-            fi
-
-            search_substring="${suffix}"
-        fi
-    done
-    result="${result}${search_substring}"
-
-    # If we can't find at least one environment variable, this field
-    # may have been intended for some other purprose and just happened
-    # to contain a question mark and resembled an environment variable.
-    # Toss out anything we did in the second stage when this happens.
-    if [[ $env_var_present != true ]]; then
-        result=${midpoint_result}
-    fi
-
-    echo "${result}"
-    return 0
-}
-
-# Ensure CircleCI environment variables can be passed in as orb parameters
-PARAM_VERSION=$(expand_circleci_env_vars "${PARAM_VERSION}")
+# Read in orb parameters
+INSTALL_PATH=$(circleci env subst "${PARAM_INSTALL_PATH}")
+VERSION=$(circleci env subst "${PARAM_VERSION}")
 
 # Check if the cosign tar file was in the CircleCI cache.
 # Cache restoration is handled in install.yml
@@ -90,12 +15,12 @@ fi
 # If there was no cache hit, go ahead and re-download the binary.
 # Tar it up to save on cache space used.
 if [[ ! -f cosign-linux-amd64 ]]; then
-    wget "https://github.com/sigstore/cosign/releases/download/v${PARAM_VERSION}/cosign-linux-amd64"
+    wget "https://github.com/sigstore/cosign/releases/download/v${VERSION}/cosign-linux-amd64"
     tar czf cosign.tar.gz cosign-linux-amd64
 fi
 
 # A cosign binary should exist at this point, regardless of whether it was obtained
 # through cache or re-downloaded. Move it to an appropriate bin directory and mark it
 # as executable.
-sudo mv cosign-linux-amd64 /usr/local/bin/cosign
-sudo chmod +x /usr/local/bin/cosign
+mv cosign-linux-amd64 "${INSTALL_PATH}/cosign"
+chmod +x "${INSTALL_PATH}/cosign"
--- src/scripts/verify_image.sh
+++ src/scripts/verify_image.sh
@@ -2,85 +2,19 @@
 
 set -e
 
-# Check to see if there are any environment variables within another environment
-# variable, and if so, expand them. This allows environment variables to be passed
-# as arguments to CircleCI orbs, which often don't interpret those variables correctly.
-# Although this function seems complex, it's safer than doing: VAR=$(eval echo "${VAR}")
-# and is less susceptible to command injection.
-#   - Supported: ${VARIABLE}
-#   - Unsupported: $VARIABLE
-expand_circleci_env_vars() {
-    search_substring=${1}
-    result=""
-
-    regex="([^}]*)[$]\{([a-zA-Z_]+[a-zA-Z0-9_]*)\}(.*)"
-    while [[ $search_substring =~ $regex ]]; do
-        prefix=${BASH_REMATCH[1]}
-        match=${BASH_REMATCH[2]}
-        suffix=${BASH_REMATCH[3]}
-
-        # if the environment variable exists, evaluate it, but
-        # guard against infinite recursion. e.g., MYVAR="\$MYVAR"
-        if [[ -n ${!match} ]] && [[ "${!match}" != "\${${match}}" ]]; then
-            repaired="${prefix}${!match}"
-            result="${result}${repaired}"
-            search_substring="${suffix}"
-        else
-            result="${result}${prefix}"
-            search_substring="${suffix}"
-        fi
-    done
-
-    midpoint_result="${result}${search_substring}"
-    search_substring="${result}${search_substring}"
-    result=""
-    env_var_present=false
-
-    # Handle the non-squiggley brace syntax: $VARIABLE.
-    regex="([^$]*)[$]([a-zA-Z_]+[a-zA-Z0-9_]*)(.*)"
-    while [[ $search_substring =~ $regex ]]; do
-        prefix=${BASH_REMATCH[1]}
-        match=${BASH_REMATCH[2]}
-        suffix=${BASH_REMATCH[3]}
-
-        # if the environment variable exists, evaluate it, but
-        # guard against infinite recursion. e.g., MYVAR="\$MYVAR"
-        if [[ -n ${!match} ]] && [[ "${!match}" != "\$${match}" ]]; then
-            repaired="${prefix}${!match}"
-            result="${result}${repaired}"
-            search_substring="${suffix}"
-            env_var_present=true
-        else
-            # If completely unset and not just an empty value, just leave it be
-            # to deal with inadequacies of aggressive mode. If the variable is
-            # actually present, but has an empty value, replace it with "".
-            if [[ -z ${!match+x} ]]; then
-                result="${result}${prefix}\$${match}"
-            else
-                result="${result}${prefix}"
-                env_var_present=true
-            fi
-
-            search_substring="${suffix}"
-        fi
-    done
-    result="${result}${search_substring}"
-
-    # If we can't find at least one environment variable, this field
-    # may have been intended for some other purprose and just happened
-    # to contain a question mark and resembled an environment variable.
-    # Toss out anything we did in the second stage when this happens.
-    if [[ $env_var_present != true ]]; then
-        result=${midpoint_result}
-    fi
-
-    echo "${result}"
-    return 0
-}
-
 # Ensure CircleCI environment variables can be passed in as orb parameters
-PARAM_IMAGE=$(expand_circleci_env_vars "${PARAM_IMAGE}")
-PARAM_PUBLIC_KEY=$(expand_circleci_env_vars "${PARAM_PUBLIC_KEY}")
+IMAGE=$(circleci env subst "${PARAM_IMAGE}")
+COSIGN_PUBLIC_KEY=${!PARAM_PUBLIC_KEY}
+
+# Cleanup makes a best effort to destroy all secrets.
+cleanup_secrets() {
+    echo "Cleaning up secrets..."
+    shred -vzu -n 10 cosign.pub 2> /dev/null || true
+    unset PARAM_PUBLIC_KEY
+    unset COSIGN_PUBLIC_KEY
+    echo "Secrets destroyed."
+}
+trap cleanup_secrets EXIT
 
 # Verify Cosign version is supported
 COSIGN_VERSION=$(cosign version --json 2>&1 | jq -r '.gitVersion' | cut -c2-)
@@ -92,14 +26,14 @@ fi
 echo "Detected Cosign major version: ${COSIGN_MAJOR_VERSION}"
 
 # Load public key, normally a base64 encoded secret within a CircleCI context
-echo "${PARAM_PUBLIC_KEY}" | base64 --decode > cosign.pub
+echo "${COSIGN_PUBLIC_KEY}" | base64 --decode > cosign.pub
 
 # Verify image signature using the public key
-echo "Verifying cosign signature for ${PARAM_IMAGE}..."
+echo "Verifying cosign signature for ${IMAGE}..."
 if [ "${COSIGN_MAJOR_VERSION}" == "1" ]; then
-    cosign verify --key cosign.pub "${PARAM_IMAGE}"
+    cosign verify --key cosign.pub "${IMAGE}"
 else
-    cosign verify --private-infrastructure=true --key cosign.pub "${PARAM_IMAGE}"
+    cosign verify --private-infrastructure=true --key cosign.pub "${IMAGE}"
 fi
 
 # Cleanup
